3a8afaf528c5e664ce5dd335c1189f088a0a501e
BegzodDev <begzodpublic@gmail.com>
2023-10-03 22:04:22 +0500

    fix bugs

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -5,65 +5,64 @@ import { ProductModel } from './product-model';
 
 @Component({
   selector: 'app-products-list',
-  templateUrl: './products-list.component.html' 
+  templateUrl: './products-list.component.html'
 })
-export class ProductsListComponent implements OnInit{
+export class ProductsListComponent implements OnInit {
   products: Product[] = [];
   productId!: string;
   searchText!: string;
   productModel!: ProductModel;
-  
 
-  
-  constructor(private productListService: ProductListServiceService){
-    
+  constructor(private productListService: ProductListServiceService) {
+
   }
-  ngOnInit(){this.reloadProductsList()
+  ngOnInit() {
+    this.reloadProductsList()
   }
-  
-  reloadProductsList(){
+
+  reloadProductsList() {
     this.productListService.getAllProductsList().subscribe({
-      next:(d:any) => {
+      next: (d: any) => {
         this.products = d;
       }
     })
   }
 
-  searchProduct(data: any){
+  searchProduct(data: any) {
     this.searchText = data;
     this.productListService.searchProductList(this.searchText).subscribe({
       next: (d: any) => {
         this.products = d;
 
-        if(d.length <= 0){
+        if (d.length <= 0) {
           alert("Product not found");
         }
       }
     });
   }
 
-  deleteProduct(product: any){
+  deleteProduct(product: any) {
     this.productListService.deleteProduct(product.id).subscribe({
-      next:(res: any) => {
+      next: (res: any) => {
         this.reloadProductsList();
       }
     });
   }
 
-  createProducts(productModel:any){
+  createProducts(productModel: any) {
     console.log("1")
     console.log(productModel)
     productModel.name = this.productModel.name;
     productModel.categoryId = this.productModel.categoryId;
     productModel.photoPath = this.productModel.photoPath;
     this.productListService.createProduct(productModel).subscribe({
-      next:(res: any) => {
+      next: (res: any) => {
         this.reloadProductsList();
       }
     })
   }
 
-  newEmit(){
+  newEmit() {
     console.log("data")
   }
 }
